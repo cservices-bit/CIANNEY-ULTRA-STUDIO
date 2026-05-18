978211281bd670b824b3b9669687d58b7b1c5a0c
CIANNEY DÉVELOPPEUR FULL STACK <cservicemobile01@gmail.com>
2026-05-18 11:23:29 +0200

    Add main components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Academy from "./components/Academy";
+import Softwares from "./components/Softwares";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Academy />
+      <Softwares />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
