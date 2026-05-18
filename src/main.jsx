18245e3e3759b3e2db38b998bd8cfe1af142e221
CIANNEY DÉVELOPPEUR FULL STACK <cservicemobile01@gmail.com>
2026-05-18 11:22:41 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-document.querySelector("#app").innerHTML = `
-  <h1 style="color:white;background:black;padding:20px;">
-    C-SERVICE STUDIO OK 🚀
-  </h1>
-`
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
